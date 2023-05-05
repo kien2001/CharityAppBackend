6354583453cb5575b98da17a022920a33ac45f49
kien2001 <kienlevan2001@gmail.com>
2023-05-05 12:41:00 +0700

    ok

--- CharityAppBL/Login/BLLogin.cs
+++ CharityAppBL/Login/BLLogin.cs
@@ -33,11 +33,12 @@ namespace CharityAppBL.Login
                 {
                     result.BadRequest(new List<string> { "Tên đăng nhập hoặc mật khẩu sai, vui lòng thử lại" });
                     return result;
-                }else if (userLogin?.IsLocked)
-                {
-                    result.BadRequest(new List<string> { "Tài khoản này đã bị khoá, vui lòng liên hệ Admin để được giải quyết." });
-                    return result;
                 }
+                //else if (userLogin?.IsLocked)
+                //{
+                //    result.BadRequest(new List<string> { "Tài khoản này đã bị khoá, vui lòng liên hệ Admin để được giải quyết." });
+                //    return result;
+                //}
                 var password = userLogin?.Password ?? "";
                 var saltPassword = userLogin?.SaltPassword ?? "";
 
